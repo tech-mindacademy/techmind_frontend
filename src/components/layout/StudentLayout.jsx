88204186767b37f416add907e266963e0d69f818
tech-mindacademy <techmindacademy70@gmail.com>
2026-05-30 12:49:41 +0530

    done

--- src/components/layout/StudentLayout.jsx
+++ src/components/layout/StudentLayout.jsx
@@ -129,6 +129,11 @@ const navItems = [
     label: "Profile",
     icon: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z",
   },
+  {
+    to: "/reviews",
+    label: "Reviews",
+    icon: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z",
+  },
 ];
 
 function NavIcon({ path }) {
