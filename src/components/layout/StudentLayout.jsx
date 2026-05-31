459d8a8ef5064cc58d8726956381110791274f7b
tech-mindacademy <techmindacademy70@gmail.com>
2026-06-01 04:17:00 +0530

    done

--- src/components/layout/StudentLayout.jsx
+++ src/components/layout/StudentLayout.jsx
@@ -134,6 +134,11 @@ const navItems = [
     label: "Reviews",
     icon: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z",
   },
+  {
+    to: "/student/refunds",
+    label: "My Refunds",
+    icon: "M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z",
+  },
 ];
 
 function NavIcon({ path }) {
