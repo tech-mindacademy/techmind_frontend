526377e3b516c979201c75e3c62140b57c3e1553
tech-mindacademy <techmindacademy70@gmail.com>
2026-06-04 15:14:20 +0530

    done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -280,7 +280,7 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div className="relative w-full max-h-[580px] overflow-hidden">
+        <div className="relative w-full max-h-[620px] overflow-hidden">
           <HeroCarousel onHasImages={setHasImages} />
         </div>
       </section>
