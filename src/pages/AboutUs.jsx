84ef1f01e519484b47d78701daa1003a9acf3113
tech-mindacademy <techmindacademy70@gmail.com>
2026-06-10 09:44:18 +0530

    done

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 import {
   GraduationCap,
   CheckCircle,
